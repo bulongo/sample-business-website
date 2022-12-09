b2883194193b4510b6c7e1a65990f7a683db41ae
bulongo <bmukkuli@gmail.com>
2022-12-09 12:11:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const burgerMenuBtn = document.querySelector(".burger-menu-btn")
+const burgerMenu = document.querySelector("#burger")
+
+document.onclick = (e) => {
+    if(e.target.id !== "burger" && e.target.className !== "burger-menu-btn"){
+        burgerMenu.classList.remove("active")
+    }
+}
+
+
+burgerMenuBtn.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active")
+    // console.log(burgerMenu.classList)
+
+    // console.log("aha,I have been clicked, dunh dunh dunh!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+})
